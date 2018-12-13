53d9b7a14c1916237a273a63534aaa7d5e15666f
fengchunsgit <1055688803@qq.com>
2018-12-13 18:21:29 +0800

    components

---
+++ src/pages/index/Child.js
@@ -0,0 +1,10 @@
+import Taro,{Component} from '@tarojs/taro'
+import {View,Text} from '@tarojs/components'
+class Child extends Component{
+  render(){
+    return (<View>child</View>)
+  }
+}
+
+export default Child
+
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text,Button} from '@tarojs/components'
+import Child from './Child'
 import './index.less'
 
 export default class Index extends Component {
@@ -23,6 +24,7 @@ export default class Index extends Component {
       <View>
         <Button onClick={this.changeName.bind(this)}>改变名字</Button>
         <Text>{this.state.name}</Text>
+        <Child />
       </View>
     )
   }
